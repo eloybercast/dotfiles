7676bb53bbbbc891b759d9653d2eb867cd6cb67f
Eloy Bermejo <eloybercast@gmail.com>
2025-06-17 21:11:56 +0200

    Remove commented line for setting Nautilus as the default file manager in setup-nautilus.sh to clean up the script.

--- scripts/setup-nautilus.sh
+++ scripts/setup-nautilus.sh
@@ -15,7 +15,6 @@ print_warning "Installing Nautilus and related packages..."
 
 sudo pacman -Syu --noconfirm nautilus gvfs gvfs-smb gvfs-afc gvfs-google
 
-# Set nautilus as default file manager for directories
 xdg-mime default nautilus.desktop inode/directory application/x-gnome-saved-search
 
 print_success "Nautilus and related utilities installed and configured."
