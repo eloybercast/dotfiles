55ae9b64a331e38614799c274e93ee9a58d146f0
Eloy Bermejo <eloybercast@gmail.com>
2025-06-21 14:04:09 +0200

    Update Waybar configuration and styling for improved icon representation and Nautilus installation check
    This commit modifies the Waybar configuration by changing workspace icons to a new symbol for better visibility. Additionally, it updates the CSS for workspace button hover effects and refines the Nautilus installation script to check for existing installation before proceeding, enhancing the overall user experience and installation process.

--- scripts/setup-nautilus.sh
+++ scripts/setup-nautilus.sh
@@ -11,10 +11,14 @@ print_error() {
     echo -e "\e[31m$1\e[0m"
 }
 
-print_warning "Installing Nautilus and related packages..."
-
-sudo pacman -Syu --noconfirm nautilus gvfs gvfs-smb gvfs-afc gvfs-google
+if command -v nautilus &> /dev/null; then
+    print_success "Nautilus is already installed."
+else
+    print_warning "Installing Nautilus and related packages..."
+    sudo pacman -Syu --noconfirm nautilus gvfs gvfs-smb gvfs-afc gvfs-google
+    print_success "Nautilus installed successfully."
+fi
 
 xdg-mime default nautilus.desktop inode/directory application/x-gnome-saved-search
 
-print_success "Nautilus and related utilities installed and configured."
+print_success "Nautilus and related utilities configured."
