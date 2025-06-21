e4183a29ff563b6b29734149330ce02aafa7baaf
Eloy Bermejo <eloybercast@gmail.com>
2025-06-21 18:02:51 +0200

    Remove unused power menu and blur scripts; update keybindings and Waybar configuration
    This commit deletes the blur-hyprland and powermenu scripts as they are no longer needed. The keybindings configuration is updated to remove the associated power menu shortcut. Additionally, the Waybar configuration is modified to point to a new location for the power menu script, ensuring proper functionality and organization.

---
+++ config/scripts/waybar/powermenu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+entries="⏻ Shutdown\n⭮ Reboot\n⏾ Suspend\n⎋ Logout"
+
+selected=$(echo -e $entries | wofi --width 250 --height 210 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+
+case $selected in
+  shutdown)
+    systemctl poweroff
+    ;;
+  reboot)
+    systemctl reboot
+    ;;
+  suspend)
+    systemctl suspend
+    ;;
+  logout)
+    hyprctl dispatch exit
+    ;;
+esac 
