b5d4f54605c5c91aee9e3993976b4af1be55f0b5
Eloy Bermejo <eloybercast@gmail.com>
2025-06-23 08:38:09 +0200

    Update powermenu script to remove the suspend option for a streamlined user experience

--- config/scripts/waybar/powermenu.sh
+++ config/scripts/waybar/powermenu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-entries="⏻ Shutdown\n⭮ Reboot\n⏾ Suspend\n⎋ Logout"
+entries="⏻ Shutdown\n⭮ Reboot\n⎋ Logout"
 
 selected=$(echo -e $entries | wofi --width 250 --height 210 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
 
